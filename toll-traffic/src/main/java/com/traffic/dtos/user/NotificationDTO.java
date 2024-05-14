a0e0df40447024222a4220a645b807da8f6d0d5f
BraG29 <braianbarrios_29@hotmail.com>
2024-05-14 14:05:36 -0300

    notifcations now have a date

--- toll-traffic/src/main/java/com/traffic/dtos/user/NotificationDTO.java
+++ toll-traffic/src/main/java/com/traffic/dtos/user/NotificationDTO.java
@@ -2,12 +2,20 @@ package com.traffic.dtos.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class NotificationDTO {
 
+<<<<<<< Updated upstream
     private Long id;
+=======
+    private LocalDate date;
+>>>>>>> Stashed changes
     private String message;
 
 }
--- toll-traffic/src/main/java/com/traffic/dtos/user/TollCustomerDTO.java
+++ toll-traffic/src/main/java/com/traffic/dtos/user/TollCustomerDTO.java
@@ -5,9 +5,11 @@ import com.traffic.dtos.account.PrePayDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TollCustomerDTO {
 
     private Long id;
