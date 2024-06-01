831a35a60a6b2112a4c0da9930fd2cb5a5e88248
ShodytheWolf <martin.antunez5@gmail.com>
2024-06-01 17:22:20 -0300

    Updated persistancy repository

--- toll-traffic/src/main/java/com/traffic/payment/domain/repository/PaymentRepositoryImplementation.java
+++ toll-traffic/src/main/java/com/traffic/payment/domain/repository/PaymentRepositoryImplementation.java
@@ -12,14 +12,16 @@ import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 public class PaymentRepositoryImplementation implements PaymentRepository {
-
-    ArrayList<User> users;
 
     @PersistenceContext
     EntityManager session;
@@ -27,18 +29,31 @@ public class PaymentRepositoryImplementation implements PaymentRepository {
 
     @PostConstruct
     public void initialize(){
-        users = new ArrayList<>();
-        //users.add(new User());
         em = session.getEntityManagerFactory().createEntityManager();
     }
 
     public void addUser(User userToAdd){
-        users.add(userToAdd);
+        //users.add(userToAdd);
         System.out.println(userToAdd.getName());
+        em.persist(userToAdd);
     }
 
-    public ArrayList<User> getAllUsers() {
-        return users;
+    public List<User> getAllUsers() {
+        //I call the criteria builder, which is the responsible for managing the queries
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+
+        //I tell Criteria builder to start a query for User
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+
+        //I tell which user class I am asking for
+        Root<User> root = cq.from(User.class);
+
+        //perform a SELECT * FROM with the class I told you (user)
+        cq.select(root);
+
+        //I finally, tell the Entity Manager to return me the result from the query
+        //that the criteria builder, built
+        return em.createQuery(cq).getResultList();
     }
 
     public User getUserById(Long id){
@@ -58,7 +73,7 @@ public class PaymentRepositoryImplementation implements PaymentRepository {
                     .add(em.merge(tollPassToAdd)));//we add and persist the toll pass we created
             em.flush(); //we flush
         }else {
-            throw new IllegalArgumentException("No existe el usuario");
+            throw new IllegalArgumentException("No se pudo actualizar las pasadas del usuario " + userDTO.getName());
         }
     }
 }
